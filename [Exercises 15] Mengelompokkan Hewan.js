f85088f33aa5e3e1ad5a814f0f313552119ea5ed
ayasaraas <sarasswati96@gmail.com>
2019-03-15 15:35:49 +0700

    Tugas Week 2

--- [Exercises 15] Mengelompokkan Hewan.js
+++ [Exercises 15] Mengelompokkan Hewan.js
@@ -1,18 +1,48 @@
 function groupAnimals(arr) {
-  var animal = arr.sort();
-  var alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  for (var i = arr.length-1; i >= 0; i--) {
+  var simpan = '';
+  for (var now = 0; now <= i; now++) {
+    var next=now + 1;
+    if (arr[now] > arr[next]) {
+      simpan = arr[next];
+      arr[next] = arr[now];
+      arr[now] = simpan;
+      simpan = '';
+    }
+  }
+  }
 
-  var pertama = 
-  for (var i = 0; i < animal.length; i++) {
-    if ()
+  var tampung = [];
+  var hasil = [];
+  tampung.push(arr[0]);
+
+  for (var i = 0; i < arr.length-1; i++) {
+    if (arr[i][0] === arr[i+1][0]) {
+      tampung.push(arr[i+1]);   
+    }
+    else { 
+      hasil.push(tampung);
+      tampung = [];
+      tampung.push(arr[i+1]);
+    }
+    if (i === arr.length-2) {
+      hasil.push(tampung);
     }
   }
 
-return animal;
+  for ( var i = 0; i<hasil.length; i++) {
+    var a = [];
+    for (var j = hasil[i].length-1; j >= 0; j--) {
+      a.push(hasil[i][j]);
+    }
+    hasil[i] = a;
+  }
+return hasil; 
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
