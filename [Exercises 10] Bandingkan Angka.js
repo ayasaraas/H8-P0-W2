3a57acd7975897d786c7a9d79c8cd3681fc2805c
ayasaraas <sarasswati96@gmail.com>
2019-03-15 09:30:05 +0700

    Tugas Week 2

--- [Exercises 10] Bandingkan Angka.js
+++ [Exercises 10] Bandingkan Angka.js
@@ -1,14 +1,14 @@
 function bandingkanAngka(angka1, angka2) {
   if (angka2 > angka1) {
-    console.log('true');
+    return true;
   }
   else if (angka2 < angka1) {
-    console.log('false')
+    return false
   }
   else {
-    console.log('-1')
+    return '-1'
   }
   return
 }
 
-console.log(bandingkanAngka(4, 4));
+console.log(bandingkanAngka(4, 2));
